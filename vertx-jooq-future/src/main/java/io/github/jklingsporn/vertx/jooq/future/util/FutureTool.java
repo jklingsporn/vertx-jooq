fd2637ec642c6694e8ce56b04271ccc902c69e83
Jens Klingsporn <klingsporn@badmonkee.de>
2017-04-24 11:48:06 +0200

    FutureTool.failedFuture

--- vertx-jooq-future/src/main/java/io/github/jklingsporn/vertx/jooq/future/util/FutureTool.java
+++ vertx-jooq-future/src/main/java/io/github/jklingsporn/vertx/jooq/future/util/FutureTool.java
@@ -14,6 +14,12 @@ import java.util.concurrent.CompletableFuture;
 public class FutureTool {
     private FutureTool(){}
 
+    /**
+     * @param blockingCodeHandler
+     * @param vertx
+     * @param <T>
+     * @return a CompletableFuture that is completed when the blocking code has been executed by Vertx.
+     */
     public static <T> CompletableFuture<T> executeBlocking(Handler<Future<T>> blockingCodeHandler,Vertx vertx){
         VertxCompletableFuture<T> future = new VertxCompletableFuture<>(vertx);
         vertx.executeBlocking(blockingCodeHandler, createCompletionHandler(future));
@@ -28,6 +34,19 @@ public class FutureTool {
                 future.completeExceptionally(h.cause());
             }
         };
+    }
+
+    /**
+     *
+     * @param e
+     * @param vertx
+     * @param <T>
+     * @return a CompletableFuture that is failed.
+     */
+    public static <T> CompletableFuture<T> failedFuture(Throwable e,Vertx vertx){
+        CompletableFuture<T> future = new VertxCompletableFuture<>(vertx);
+        future.completeExceptionally(e);
+        return future;
     }
 
 }
--- vertx-jooq-future/src/test/java/io/github/jklingsporn/vertx/jooq/future/util/FutureToolTest.java
+++ vertx-jooq-future/src/test/java/io/github/jklingsporn/vertx/jooq/future/util/FutureToolTest.java
@@ -1,8 +1,10 @@
 package io.github.jklingsporn.vertx.jooq.future.util;
 
 import io.vertx.core.Vertx;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -14,6 +16,17 @@ public class FutureToolTest {
     public void executeBlockingShouldCompleteFuture() throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(1);
         FutureTool.executeBlocking(h -> countDownLatch.countDown(), Vertx.vertx());
+        countDownLatch.await(1, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void failedFutureShouldCompleteWithException() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        CompletableFuture<Void> failedFuture = FutureTool.failedFuture(new RuntimeException(), Vertx.vertx());
+        failedFuture.whenComplete((v,ex)->{
+            Assert.assertEquals(RuntimeException.class, ex.getCause().getClass());
+            countDownLatch.countDown();
+        });
         countDownLatch.await(1, TimeUnit.SECONDS);
     }
 }
