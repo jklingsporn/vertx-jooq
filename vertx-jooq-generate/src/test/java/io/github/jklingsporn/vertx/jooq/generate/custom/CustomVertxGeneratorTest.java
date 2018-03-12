f936a65fd1cd52682abfaec3082a89dc31e9d820
Jens Klingsporn <klingsporn@badmonkee.de>
2018-03-12 10:28:46 +0100

    fix compilation fails when interfaces generation is set to false #37

--- vertx-jooq-generate/src/test/java/io/github/jklingsporn/vertx/jooq/generate/custom/CustomVertxGeneratorTest.java
+++ vertx-jooq-generate/src/test/java/io/github/jklingsporn/vertx/jooq/generate/custom/CustomVertxGeneratorTest.java
@@ -2,7 +2,9 @@ package io.github.jklingsporn.vertx.jooq.generate.custom;
 
 import io.github.jklingsporn.vertx.jooq.generate.AbstractVertxGeneratorTest;
 import io.github.jklingsporn.vertx.jooq.generate.JDBCDatabaseConfigurationProvider;
+import io.github.jklingsporn.vertx.jooq.generate.VertxGeneratorStrategy;
 import io.github.jklingsporn.vertx.jooq.generate.classic.JDBCClassicVertxGeneratorStrategy;
+import org.jooq.util.jaxb.Configuration;
 
 /**
  * Created by jklingsporn on 17.09.16.
@@ -11,7 +13,15 @@ public class CustomVertxGeneratorTest extends AbstractVertxGeneratorTest{
 
 
     public CustomVertxGeneratorTest() {
-        super(CustomVertxGenerator.class, JDBCClassicVertxGeneratorStrategy.class,"classic.jdbc.custom", JDBCDatabaseConfigurationProvider.getInstance());
+        super(CustomVertxGenerator.class, JDBCClassicVertxGeneratorStrategy.class,"classic.jdbc.custom", new JDBCDatabaseConfigurationProvider(){
+            @Override
+            public Configuration createGeneratorConfig(String generatorName, String packageName, Class<? extends VertxGeneratorStrategy> generatorStrategy) {
+                org.jooq.util.jaxb.Configuration conf =  super.createGeneratorConfig(generatorName, packageName, generatorStrategy);
+                //see if generator without interfaces produces compilable classes
+                conf.getGenerator().getGenerate().setInterfaces(false);
+                return conf;
+            }
+        });
     }
 
 }
