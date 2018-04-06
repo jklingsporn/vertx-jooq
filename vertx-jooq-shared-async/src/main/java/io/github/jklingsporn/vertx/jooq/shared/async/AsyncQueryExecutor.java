2215d14f698f7baba20a300c2eb93eac85d887be
Jens Klingsporn <klingsporn@badmonkee.de>
2018-04-06 11:41:17 +0200

    extend BasicQueryExecutor

--- vertx-jooq-shared-async/src/main/java/io/github/jklingsporn/vertx/jooq/shared/async/AsyncQueryExecutor.java
+++ vertx-jooq-shared-async/src/main/java/io/github/jklingsporn/vertx/jooq/shared/async/AsyncQueryExecutor.java
@@ -1,7 +1,7 @@
 package io.github.jklingsporn.vertx.jooq.shared.async;
 
+import io.github.jklingsporn.vertx.jooq.shared.internal.BasicQueryExecutor;
 import org.jooq.DSLContext;
-import org.jooq.Query;
 import org.jooq.Record;
 import org.jooq.ResultQuery;
 
@@ -12,7 +12,7 @@ import java.util.function.Function;
  * @param <FIND_ONE_JSON> a type to represent one <code>JsonObject</code>.
  * @param <EXECUTE> the result type returned for all insert, update and delete-operations. This varies on the AsyncQueryExecutor-subtypes, e.g. {@code Future<Integer>}.
  */
-public interface AsyncQueryExecutor<FIND_MANY_JSON, FIND_ONE_JSON, EXECUTE> {
+public interface AsyncQueryExecutor<FIND_MANY_JSON, FIND_ONE_JSON, EXECUTE> extends BasicQueryExecutor<EXECUTE>{
 
     /**
      * Executes the given query and returns the results as a List of JsonObjects asynchronously.
@@ -30,13 +30,5 @@ public interface AsyncQueryExecutor<FIND_MANY_JSON, FIND_ONE_JSON, EXECUTE> {
      * @return the result or <code>null</code>.
      */
     public <Q extends Record> FIND_ONE_JSON findOneJson(Function<DSLContext, ? extends ResultQuery<Q>> queryFunction);
-
-    /**
-     * Executes a query and returns the result of the execution (usually an <code>Integer</code>-value)
-     * @param queryFunction
-     * @return the result type returned for all insert, update and delete-operations.
-     * @see Query#execute()
-     */
-    EXECUTE execute(Function<DSLContext, ? extends Query> queryFunction);
 
 }
--- vertx-jooq-shared-reactive/src/main/java/io/github/jklingspon/vertx/jooq/shared/reactive/ReactiveQueryExecutor.java
+++ vertx-jooq-shared-reactive/src/main/java/io/github/jklingspon/vertx/jooq/shared/reactive/ReactiveQueryExecutor.java
@@ -1,7 +1,7 @@
 package io.github.jklingspon.vertx.jooq.shared.reactive;
 
+import io.github.jklingsporn.vertx.jooq.shared.internal.BasicQueryExecutor;
 import org.jooq.DSLContext;
-import org.jooq.Query;
 import org.jooq.Record;
 import org.jooq.ResultQuery;
 
@@ -12,7 +12,7 @@ import java.util.function.Function;
  * @param <FIND_ONE_ROW> a type to represent one <code>Row</code>.
  * @param <EXECUTE> the result type returned for all insert, update and delete-operations. This varies on the ReactiveQueryExecutor-subtypes, e.g. {@code Future<Integer>}.
  */
-public interface ReactiveQueryExecutor<FIND_MANY_ROW, FIND_ONE_ROW, EXECUTE> {
+public interface ReactiveQueryExecutor<FIND_MANY_ROW, FIND_ONE_ROW, EXECUTE>  extends BasicQueryExecutor<EXECUTE> {
 
     /**
      * Executes the given query and returns the results as a List of {@code Row}s asynchronously.
@@ -30,13 +30,5 @@ public interface ReactiveQueryExecutor<FIND_MANY_ROW, FIND_ONE_ROW, EXECUTE> {
      * @return the result or <code>null</code>.
      */
     public <Q extends Record> FIND_ONE_ROW findOneRow(Function<DSLContext, ? extends ResultQuery<Q>> queryFunction);
-
-    /**
-     * Executes a query and returns the result of the execution (usually an <code>Integer</code>-value)
-     * @param queryFunction
-     * @return the result type returned for all insert, update and delete-operations.
-     * @see Query#execute()
-     */
-    EXECUTE execute(Function<DSLContext, ? extends Query> queryFunction);
 
 }
