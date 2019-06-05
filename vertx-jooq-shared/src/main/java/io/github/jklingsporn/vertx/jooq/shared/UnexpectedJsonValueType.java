b89c578225e91da186b7e417a2f85614050935d7
Oded Arbel <oded@geek.co.il>
2019-06-05 20:07:05 +0300

    move jsonifying to its a method so we can do more stuff

--- vertx-jooq-shared/src/main/java/io/github/jklingsporn/vertx/jooq/shared/UnexpectedJsonValueType.java
+++ vertx-jooq-shared/src/main/java/io/github/jklingsporn/vertx/jooq/shared/UnexpectedJsonValueType.java
@@ -11,8 +11,18 @@ public class UnexpectedJsonValueType extends ClassCastException {
   private static final long serialVersionUID = 8727637165178779604L;
   
   public UnexpectedJsonValueType(String fieldName, String fieldType, ClassCastException cause) {
-    super("Invalid JSON type provided for field '" + fieldName + "', expecting: " +
-        fieldType.replaceAll("\\w+\\.","").toLowerCase()); // trim field type to something JSON lovers would recognize
+    super("Invalid JSON type provided for field '" + fieldName + "', expecting: " + jsonifyType(fieldType));
+  }
+  
+  /**
+   * Trim field type to something JSON lovers would recognize.
+   * @param type The Java type
+   * @return a not exactly JSON type, but close
+   */
+  private static String jsonifyType(String type) {
+    return type
+      .replaceAll("\\w+\\.","") // remove package
+      .replace("Json",""); // handle Vert.x's Json(Object|Array)
   }
   
 }
