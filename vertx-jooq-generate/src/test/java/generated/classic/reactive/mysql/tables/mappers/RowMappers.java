013a6a453c7125f97942ca8b4e88269c7b296616
Benjamin Berman <benjamin.s.berman@gmail.com>
2020-12-07 10:44:26 -0800

    Fix import of mysqlclient implementation classes in a postgres-only project.

--- vertx-jooq-generate/src/test/java/generated/classic/reactive/mysql/tables/mappers/RowMappers.java
+++ vertx-jooq-generate/src/test/java/generated/classic/reactive/mysql/tables/mappers/RowMappers.java
@@ -17,15 +17,25 @@ public class RowMappers {
             pojo.setSomeregularnumber(row.getInteger("someRegularNumber"));
             pojo.setSomedouble(row.getDouble("someDouble"));
             pojo.setSomeenum(java.util.Arrays.stream(generated.classic.reactive.mysql.enums.SomethingSomeenum.values()).filter(td -> td.getLiteral().equals(row.getString("someEnum"))).findFirst().orElse(null));
-            if (!(row instanceof io.vertx.mysqlclient.impl.MySQLRowImpl) && row.get(Object.class, "someJsonObject") instanceof io.vertx.core.json.JsonObject) {
-                pojo.setSomejsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someJsonObject")));
-            } else {
+            try {
+                if (row.get(Object.class, "someJsonObject") instanceof io.vertx.core.json.JsonObject) {
+                    pojo.setSomejsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someJsonObject")));
+                } else {
+                    String someJsonObjectString = row.getString("someJsonObject");
+                    pojo.setSomejsonobject(someJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someJsonObjectString));
+                }
+            } catch (UnsupportedOperationException t) {
                 String someJsonObjectString = row.getString("someJsonObject");
                 pojo.setSomejsonobject(someJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someJsonObjectString));
             }
-            if (!(row instanceof io.vertx.mysqlclient.impl.MySQLRowImpl) && row.get(Object.class, "someJsonArray") instanceof io.vertx.core.json.JsonArray) {
-                pojo.setSomejsonarray(row.get(io.vertx.core.json.JsonArray.class,row.getColumnIndex("someJsonArray")));
-            } else {
+            try {
+                if (row.get(Object.class, "someJsonArray") instanceof io.vertx.core.json.JsonArray) {
+                    pojo.setSomejsonarray(row.get(io.vertx.core.json.JsonArray.class,row.getColumnIndex("someJsonArray")));
+                } else {
+                    String someJsonArrayString = row.getString("someJsonArray");
+                    pojo.setSomejsonarray(someJsonArrayString == null ? null : new io.vertx.core.json.JsonArray(someJsonArrayString));
+                }
+            } catch (UnsupportedOperationException t) {
                 String someJsonArrayString = row.getString("someJsonArray");
                 pojo.setSomejsonarray(someJsonArrayString == null ? null : new io.vertx.core.json.JsonArray(someJsonArrayString));
             }
@@ -39,9 +49,14 @@ public class RowMappers {
             generated.classic.reactive.mysql.tables.pojos.Somethingcomposite pojo = new generated.classic.reactive.mysql.tables.pojos.Somethingcomposite();
             pojo.setSomeid(row.getInteger("someId"));
             pojo.setSomesecondid(row.getInteger("someSecondId"));
-            if (!(row instanceof io.vertx.mysqlclient.impl.MySQLRowImpl) && row.get(Object.class, "someJsonObject") instanceof io.vertx.core.json.JsonObject) {
-                pojo.setSomejsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someJsonObject")));
-            } else {
+            try {
+                if (row.get(Object.class, "someJsonObject") instanceof io.vertx.core.json.JsonObject) {
+                    pojo.setSomejsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someJsonObject")));
+                } else {
+                    String someJsonObjectString = row.getString("someJsonObject");
+                    pojo.setSomejsonobject(someJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someJsonObjectString));
+                }
+            } catch (UnsupportedOperationException t) {
                 String someJsonObjectString = row.getString("someJsonObject");
                 pojo.setSomejsonobject(someJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someJsonObjectString));
             }
--- vertx-jooq-generate/src/test/java/generated/rx/reactive/guice/vertx/tables/mappers/RowMappers.java
+++ vertx-jooq-generate/src/test/java/generated/rx/reactive/guice/vertx/tables/mappers/RowMappers.java
@@ -17,22 +17,37 @@ public class RowMappers {
             pojo.setSomeregularnumber(row.getInteger("someRegularNumber"));
             pojo.setSomedouble(row.getDouble("someDouble"));
             pojo.setSomeenum(java.util.Arrays.stream(generated.rx.reactive.guice.vertx.enums.Someenum.values()).filter(td -> td.getLiteral().equals(row.getString("someEnum"))).findFirst().orElse(null));
-            if (!(row instanceof io.vertx.mysqlclient.impl.MySQLRowImpl) && row.get(Object.class, "someJsonObject") instanceof io.vertx.core.json.JsonObject) {
-                pojo.setSomejsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someJsonObject")));
-            } else {
+            try {
+                if (row.get(Object.class, "someJsonObject") instanceof io.vertx.core.json.JsonObject) {
+                    pojo.setSomejsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someJsonObject")));
+                } else {
+                    String someJsonObjectString = row.getString("someJsonObject");
+                    pojo.setSomejsonobject(someJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someJsonObjectString));
+                }
+            } catch (UnsupportedOperationException t) {
                 String someJsonObjectString = row.getString("someJsonObject");
                 pojo.setSomejsonobject(someJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someJsonObjectString));
             }
             pojo.setSomecustomjsonobject(generated.rx.reactive.guice.vertx.tables.converters.Converters.IO_GITHUB_JKLINGSPORN_VERTX_JOOQ_GENERATE_CONVERTER_SOMEJSONPOJOCONVERTER_INSTANCE.pgConverter().from(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someCustomJsonObject"))));
-            if (!(row instanceof io.vertx.mysqlclient.impl.MySQLRowImpl) && row.get(Object.class, "someJsonArray") instanceof io.vertx.core.json.JsonArray) {
-                pojo.setSomejsonarray(row.get(io.vertx.core.json.JsonArray.class,row.getColumnIndex("someJsonArray")));
-            } else {
+            try {
+                if (row.get(Object.class, "someJsonArray") instanceof io.vertx.core.json.JsonArray) {
+                    pojo.setSomejsonarray(row.get(io.vertx.core.json.JsonArray.class,row.getColumnIndex("someJsonArray")));
+                } else {
+                    String someJsonArrayString = row.getString("someJsonArray");
+                    pojo.setSomejsonarray(someJsonArrayString == null ? null : new io.vertx.core.json.JsonArray(someJsonArrayString));
+                }
+            } catch (UnsupportedOperationException t) {
                 String someJsonArrayString = row.getString("someJsonArray");
                 pojo.setSomejsonarray(someJsonArrayString == null ? null : new io.vertx.core.json.JsonArray(someJsonArrayString));
             }
-            if (!(row instanceof io.vertx.mysqlclient.impl.MySQLRowImpl) && row.get(Object.class, "someVertxJsonObject") instanceof io.vertx.core.json.JsonObject) {
-                pojo.setSomevertxjsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someVertxJsonObject")));
-            } else {
+            try {
+                if (row.get(Object.class, "someVertxJsonObject") instanceof io.vertx.core.json.JsonObject) {
+                    pojo.setSomevertxjsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someVertxJsonObject")));
+                } else {
+                    String someVertxJsonObjectString = row.getString("someVertxJsonObject");
+                    pojo.setSomevertxjsonobject(someVertxJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someVertxJsonObjectString));
+                }
+            } catch (UnsupportedOperationException t) {
                 String someVertxJsonObjectString = row.getString("someVertxJsonObject");
                 pojo.setSomevertxjsonobject(someVertxJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someVertxJsonObjectString));
             }
@@ -51,9 +66,14 @@ public class RowMappers {
             generated.rx.reactive.guice.vertx.tables.pojos.Somethingcomposite pojo = new generated.rx.reactive.guice.vertx.tables.pojos.Somethingcomposite();
             pojo.setSomeid(row.getInteger("someId"));
             pojo.setSomesecondid(row.getInteger("someSecondId"));
-            if (!(row instanceof io.vertx.mysqlclient.impl.MySQLRowImpl) && row.get(Object.class, "someJsonObject") instanceof io.vertx.core.json.JsonObject) {
-                pojo.setSomejsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someJsonObject")));
-            } else {
+            try {
+                if (row.get(Object.class, "someJsonObject") instanceof io.vertx.core.json.JsonObject) {
+                    pojo.setSomejsonobject(row.get(io.vertx.core.json.JsonObject.class,row.getColumnIndex("someJsonObject")));
+                } else {
+                    String someJsonObjectString = row.getString("someJsonObject");
+                    pojo.setSomejsonobject(someJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someJsonObjectString));
+                }
+            } catch (UnsupportedOperationException t) {
                 String someJsonObjectString = row.getString("someJsonObject");
                 pojo.setSomejsonobject(someJsonObjectString == null ? null : new io.vertx.core.json.JsonObject(someJsonObjectString));
             }
