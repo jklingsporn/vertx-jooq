9f2d4c507d84138f907a8e277e9ffb51c29ca241
Jens Klingsporn <jens.klingsporn@gmail.com>
2020-12-11 13:57:16 +0100

    remove setter return type from the API

--- vertx-jooq-shared/src/main/java/io/github/jklingsporn/vertx/jooq/shared/internal/VertxPojo.java
+++ vertx-jooq-shared/src/main/java/io/github/jklingsporn/vertx/jooq/shared/internal/VertxPojo.java
@@ -26,17 +26,17 @@ public interface VertxPojo {
      * For internal purposes only.<br>
      * Function to safely set a value inside a POJO. Helps users to find bugs if they accidentally put a value into
      * a <{@code JsonObject} with a wrong type.
-     * @param setter the setter of this POJO
-     * @param getter the getter to obtain the value from the <{@code JsonObject}
-     * @param fieldName the name of the field
-     * @param expectedFieldType the type of the field
+     * @param pojoSetter the setter of this POJO's property
+     * @param jsonGetter the function to obtain the value from the <{@code JsonObject}
+     * @param fieldName the name of the POJO / JSON-property
+     * @param expectedFieldType the type of the property/field
      * @param <T> the value type
-     * @param <U> the return type of the setter
      * @see <a href="https://github.com/jklingsporn/vertx-jooq/pull/110">Related PR</a>
+     * @see <a href="https://github.com/jklingsporn/vertx-jooq/issues/170">Related Issue</a>
      */
-    public static <T,U> void setOrThrow(Function<T,U> setter, Function<String,T> getter, String fieldName, String expectedFieldType){
+    public static <T> void setOrThrow(Function<T,?> pojoSetter, Function<String,T> jsonGetter, String fieldName, String expectedFieldType){
         try {
-            setter.apply(getter.apply(fieldName));
+            pojoSetter.apply(jsonGetter.apply(fieldName));
         } catch (java.lang.ClassCastException e) {
             throw new UnexpectedJsonValueTypeException(fieldName,expectedFieldType,e);
         }
